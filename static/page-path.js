5877f08df733c098b909383857cd8954a3d5f819
You <397442219@qq.com>
2020-05-11 19:27:53 +0800

    first commit

---
+++ static/page-path.js
@@ -0,0 +1,34 @@
+const PAGE_PICTURE_BOOK = '/pages/pictureBook/index';
+const PAGE_READING_PROMOTION = '/pages/readingPromotion/index'
+const PAGE_PROMOTION_DETAIL = '/pages/readingPromotion/promotionDetail'
+const PAGE_NICE_PROMOTION = '/pages/readingPromotion/nicePromotion'
+const PAGE_MEMBER_MESSAGE = '/pages/my/memberMessage'
+const PAGE_MEMBER_CARD = '/pages/my/memberCard'
+const PAGE_BOOKSELF = '/pages/my/bookself'
+const PAGE_MEMBER_LEVEL = '/pages/my/memberlevel'
+const PAGE_LEVEL_DETAIL = '/pages/my/levelDetail'
+const PAGE_ABOUT_ME = '/pages/my/aboutMe'
+const PAGE_REGIESTER = '/pages/my/register'
+const PAGE_CLOCK_DETAIL = '/pages/clockcard/clockDetail'
+const PAGE_READ_BOOK = '/pages/index/readbook'
+const PAGE_LISTEN_BOOK = '/pages/index/listenbook'
+const PAGE_ANSWER_QUESTION = '/pages/index/answerquestion'
+
+
+module.exports = {
+	PAGE_PICTURE_BOOK,
+	PAGE_READING_PROMOTION,
+	PAGE_PROMOTION_DETAIL,
+	PAGE_NICE_PROMOTION,
+	PAGE_MEMBER_MESSAGE,
+	PAGE_MEMBER_CARD,
+	PAGE_BOOKSELF,
+	PAGE_MEMBER_LEVEL,
+	PAGE_LEVEL_DETAIL,
+	PAGE_ABOUT_ME,
+	PAGE_REGIESTER,
+	PAGE_CLOCK_DETAIL,
+	PAGE_READ_BOOK,
+	PAGE_LISTEN_BOOK,
+	PAGE_ANSWER_QUESTION
+}
